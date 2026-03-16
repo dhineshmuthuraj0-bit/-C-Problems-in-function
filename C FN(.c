874ac95a3a9a1ce3daa65976bb6f85d88c0d2ff2
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-16 09:51:46 +0530

    Create C FN(.c

---
+++ C FN(.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int rev(int n) {
+    int r=0;
+    while (n>0) {
+        r = r*10 + n%10;
+        n = n/10;
+    }
+    return r;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n==rev(n)) {
+        printf("Parildrome");
+    }
+    else {
+        printf("Not parildrome");
+    }
+    return 0;
+}
