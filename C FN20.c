1177b23543310de70f8df548dcd9b8c9f04d9c90
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-17 09:30:04 +0530

    Create C FN20.c

---
+++ C FN20.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+float add(float a,float b){return a+b;}
+float sub(float a,float b){return a-b;}
+float mul(float a,float b){return a*b;}
+float div(float a,float b){return a/b;}
+
+int main()
+{
+    float a,b;
+    char c;
+    scanf("%f%f %c",&a,&b,&c);
+    switch(c)
+    {
+        case '+': printf("%.f",add(a,b)); break;
+        case '-': printf("%.f",sub(a,b)); break;
+        case '*': printf("%.f",mul(a,b)); break;
+        case '/': printf("%.f",div(a,b)); break;
+    }
+    return 0;
+}
